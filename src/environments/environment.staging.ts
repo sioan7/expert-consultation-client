2d505679efaef00c732cfb3e52bcb59c5d6cff0d
George Bejan <bejangeorge@gmail.com>
2020-06-16 18:58:43 +0300

    Update backend API url

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'http://expert-consultation-api-staging.eu-west-1.elasticbeanstalk.com/api',
+  api_url: 'https://api.legiinlucru.ro/api',
 };
